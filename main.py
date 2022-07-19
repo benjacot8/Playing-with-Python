0e19187a099fa8a1f3533bc6c6b75294c89d4ec2
benjacot8 <benjct@gmail.com>
2022-07-18 21:29:50 -0700

    Added print statement
    "hello there Dad"

---
+++ main.py
@@ -0,0 +1,10 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+class main():
+
+def print_hello():
+    # Use a breakpoint in the code line below to debug your script.
+    print('hello there Dad')  # Press Ctrl+F8 to toggle the breakpoint.
